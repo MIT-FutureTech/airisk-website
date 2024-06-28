2689f0585d0593a15a670c540a8a7437d40eb878
Andrew Lucas <andrewlucasgs@gmail.com>
2024-06-28 03:15:43 -0300

    Add Table

---
+++ components/Cell.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="p-2"> <template v-if="cell.formattedValue?.length > 100 && !seeAll">
+            {{ cell.formattedValue.substring(0, 100) }}... <button @click="seeAll = !seeAll">See All</button>
+        </template>
+        <template v-else>
+            {{ cell.formattedValue }} <button v-if="cell.formattedValue?.length > 100" @click="seeAll = !seeAll">See Less</button>
+        </template>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    cell: Object
+})
+
+const seeAll = ref(false)
+
+
+
+
+
+</script>
--- components/TableBlock.vue
+++ components/TableBlock.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="mt-8 bg-gray-500 h-96">
-        {{ data }}
+    <TableAIRiskDatabase :data="data"  v-if="data.table_name === 'Database table'" />
+    <div v-else class="mt-8 bg-gray-500 h-96">
         
     </div>
 </template>
