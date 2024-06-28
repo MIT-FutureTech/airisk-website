0a7f572ec2cbf88dc1df1982f412cc49b97037f4
Andrew Lucas <andrewlucasgs@gmail.com>
2024-06-27 23:24:53 -0300

    Update url

--- plugins/directus.js
+++ plugins/directus.js
@@ -1,6 +1,6 @@
 import { createDirectus, rest, readItem, readItems } from '@directus/sdk';
 
-const directus = createDirectus('http://localhost:8055/').with(rest());
+const directus = createDirectus('http://128.52.139.178:8055/').with(rest());
 
 export default defineNuxtPlugin(() => {
 	return {
