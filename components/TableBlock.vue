778b8045b19b20ae8b0d6da20bb41200c54dc8ec
Andrew Lucas <andrewlucasgs@gmail.com>
2024-07-02 23:18:22 -0300

    Add TableBlock

--- components/TableBlock.vue
+++ components/TableBlock.vue
@@ -1,6 +1,8 @@
 <template>
-    <TableIncludedResources />
-    
+    <component :data="data" v-if="Object.keys(TableComponents).includes(data.table_name)" :is="TableComponents[data.table_name]" />
+    <div v-else class="mt-8 bg-gray-500 h-96">
+        {{ data }} is not supported yet. {{ Object.keys(TableComponents)}}
+    </div>
 </template>
 
 <script setup>
@@ -11,4 +13,8 @@ defineProps({
         required: true,
     },
 });
+
+const TableComponents = {
+    'AI Risk Database v0.1': resolveComponent('TableAIRiskDatabase'),
+};
 </script>
