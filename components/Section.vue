7ebdeafc83341bf404983d7eff20c7e4349447fc
Andrew Lucas <andrewlucasgs@gmail.com>
2024-07-12 09:05:32 -0300

    Darker the background section

--- components/Section.vue
+++ components/Section.vue
@@ -3,7 +3,7 @@
     :id="data.title.toLowerCase().replaceAll(' ', '-')"
         :class="{
             'bg-white': index % 2 === 0,
-            'bg-[#FAFAFA]': index % 2 === 1,
+            'bg-gray-200/70': index % 2 === 1,
         }"
     >
         <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
